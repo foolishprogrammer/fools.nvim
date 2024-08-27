c6167281d3053b5f69a13d9a8c31abc5f7ddfbc1
foolishprogrammer <ferry.foolish.programmer@gmail.com>
2024-08-27 17:18:48 +0700

    [INIT] Developmment Branch

--- init.lua
+++ init.lua
@@ -1,13 +1,11 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-require 'options'
+require('options')
 
-require 'keymaps'
+require('keymaps')
 
-require 'lazy_bootstrap'
+require('lazy_bootstrap')
 
-require 'lazy_plugins'
+require('lazy_plugins')
+
